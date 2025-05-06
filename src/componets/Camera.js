35b635bd35007ec36d2523cad41e2d39e37fad1d
Neoxy <neox9487@gmail.com>
2025-05-06 11:48:31 +0800

    update

---
+++ src/componets/Camera.js
@@ -0,0 +1,26 @@
+import React, { use } from "react";
+import { useState, useEffect } from "react";
+
+function Camera({port}) {
+  const [cameraSettings, setCameraSettings] = useState({
+
+  })
+
+  useEffect(
+    ()=>{
+      setCameraSettings({
+        
+      })
+    }
+  )
+
+  const handleInputChange = () => {
+
+  };
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
