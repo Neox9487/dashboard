212d30421fb93c873552cd1f849665a66cee1e84
jay.agyoyo@gmail.com <neox9487@gmail.com>
2025-05-09 14:49:30 +0800

    add Database

---
+++ backend/database/db_operation.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+class MySQLHelper():
+  def __init__(self):
+    self.db = mysql.connector.connect(
+      host='localhost',   
+      user='root',    
+      password='password',
+      database = "dashboard"
+    ) 
+    self.mycursor = self.db.cursor()
